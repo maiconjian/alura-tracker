0082d001a0a8855af39d49d107a22e8f8c21f1c2
maiconjian <maiconjiantelles@gmail.com>
2023-05-05 15:09:09 -0300

    commit grupo 3: Criação da lista de tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
- <main class="columns is-gapless is-multiline">
-     <div class="column is-one-quarter">
-      <BarraLateral/>
-     </div>
-     <div class="column is-three-quarter">
-      <Formulario/>
-<!--         Lista de tarefa-->
-     </div>
- </main>
+    <main class="columns is-gapless is-multiline">
+        <div class="column is-one-quarter">
+            <BarraLateral/>
+        </div>
+        <div class="column is-three-quarter">
+            <Formulario @aoSalvarTarefa="salvarTarefa"/>
+            <div class="lista">
+                <Tarefa v-for="(tarefa,index) in tarefas" :key="index" :tarefa="tarefa"/>
+                <Box v-if="listaEstaVazia">
+                    Você não esta muito produtivo hoje :(
+                </Box>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import BarraLateral from "@/components/BarraLateral.vue";
 import Formulario from "@/components/Formulario.vue";
+import Tarefa from "@/components/Tarefa.vue";
+import ITarefa from "@/interfaces/ITarefa";
+import Box from "@/components/Box.vue";
 
 export default defineComponent({
-  name: 'App',
+    name: 'App',
     components: {
-      BarraLateral,
-      Formulario},
+        Box,
+        BarraLateral,
+        Formulario,
+        Tarefa
+    },
+    data() {
+        return {
+            tarefas: [] as ITarefa[],
+        }
+    },
+    computed:{
+        listaEstaVazia():boolean{
+            return this.tarefas.length ===0
+        }
+    },
+    methods: {
+        salvarTarefa(tarefa: ITarefa) {
+            this.tarefas.push(tarefa);
+        }
+    }
 });
 </script>
 
 <style>
+.lista {
+    padding: 1.5rem;
+
+}
 
 </style>
